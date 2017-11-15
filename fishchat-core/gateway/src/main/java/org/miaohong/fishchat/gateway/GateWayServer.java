867989993c55e59c2e063cc904faf97edcda3fc6
harold.miao <harold.miao@vipshop.com>
2017-11-15 19:59:42 +0800

    init

---
+++ fishchat-core/gateway/src/main/java/org/miaohong/fishchat/gateway/GateWayServer.java
@@ -0,0 +1,13 @@
+package org.miaohong.fishchat.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayServer.class, args);
+	}
+
+}
