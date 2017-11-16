654c500869fed29b41cea9d0f324832de9ff971e
harold.miao <harold.miao@vipshop.com>
2017-11-16 22:57:52 +0800

    init

--- fishchat-server/gateway/src/main/java/org/miaohong/fishchat/gateway/GateWayServer.java
+++ fishchat-server/gateway/src/main/java/org/miaohong/fishchat/gateway/GateWayServer.java
@@ -1,13 +1,15 @@
 package org.miaohong.fishchat.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GateWayServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GateWayServer.class, args);
+		SpringApplication app = new SpringApplication(GateWayServer.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
-
 }
